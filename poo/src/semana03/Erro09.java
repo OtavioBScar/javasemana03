dbd9e126877fca06b87be808792c3df4d02e1ce1
OtavioBScar <otavio.scarpelline@gmail.com>
2024-08-23 21:22:28 -0300

    melhoria erro09

--- poo/src/semana03/Erro09.java
+++ poo/src/semana03/Erro09.java
@@ -2,7 +2,6 @@ package semana03;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class Erro09 {
 
@@ -10,13 +9,10 @@ public class Erro09 {
 			throws Exception {
 				BufferedReader br = new BufferedReader(
 						new FileReader("nomes.txt"));
+				for(int i = 0; i < 7; i++) {
 				System.out.println(br.readLine());
-				System.out.println(br.readLine());
-				System.out.println(br.readLine());
-				System.out.println(br.readLine());
-				System.out.println(br.readLine());
-				System.out.println(br.readLine());
-				System.out.println(br.readLine());
+				}
+				System.out.println(br.lines());
 				br.close();
 			}
 }
